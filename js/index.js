fad4e12434f14a5b2ab51f2c609d0850a4031386
olucheye <seyefree2rhyme@yahoo.com>
2020-04-23 11:32:04 +0100

    basic fetch fixed

--- js/index.js
+++ js/index.js
@@ -1,7 +1,44 @@
-//call function here
+//call function here & button click
+//pruneLink(); - Cannot call the function because the submit button does
 
 
-//define global variables
 
+
+
+//endpoint & url
+const apiEndpoint = 'https://rel.ink/api/links/';
+const domain = "https://rel.ink/";
+
+//global variables
+const longURL = document.querySelector('#url');
+const uri = "https://www.quickteller.com";
+
+
+/*
+//prevent default from button
+const getURL = document.querySelector(".submitURL").addEventListener("click", function(event){
+
+    pruneLink();
+    event.preventDefault();
+    //set provided url to a variable.
+
+  });
+
+*/
 
 //define async/await function
+async function pruneLink(uri){
+
+    let res = await fetch(apiEndpoint, {
+        method: 'POST',
+        body: JSON.stringify({url: uri}),
+        headers: {
+            "Content-type": "application/json"
+        }
+    });
+
+    let data = await res.json();
+    console.log(data);
+}
+
+pruneLink(uri);
